bad9f01056f48350d0b11ab4cb7fc067d5fccda9
Cheng Vue <cvu3@ucsc.edu>
2018-11-20 05:24:12 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,12 @@ def countdown(num)
   while num > 0
     puts "#{num} SECOND(S)!"
     num -= 1
+    countdown_with_sleep(1)
   end
 
   "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+  sleep(num)
 end
